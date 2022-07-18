812cc6025d739e1acea8a56d03af700f2c10dfac
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-07-18 11:36:18 +0545

    feat : initial project done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
+import Note from "./components/Note";
 
 
-function App() {
+function App(props) {
+
+  const [notes, setNotes] = useState(props.notes);
   return (
-    <div className="App">
+    <div>
       <h1>Notes App</h1>
-      
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
     </div>
   );
 }
