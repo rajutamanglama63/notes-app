ac9f46211510f58f65e00b964d2aa372bf51556c
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-07-18 13:35:46 +0545

    feat : form feature added

--- src/App.js
+++ src/App.js
@@ -5,9 +5,26 @@ import Note from "./components/Note";
 function App(props) {
 
   const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    let note = {
+      id : notes.length + 1,
+      content : newNote,
+      date : new Date().toISOString(),
+      important : Math.random() < 0.5  ? true : false
+    }
+    setNotes([...notes, note])
+    setNewNote("");
+  }
   return (
     <div>
       <h1>Notes App</h1>
+      <form onSubmit={submitHandler}>
+        <input placeholder="enter new note here..." value={newNote} onChange={(e) => setNewNote(e.target.value)} /> 
+        <button type="submit">create note</button>
+      </form>
       <ul>
         {notes.map((note) => (
           <Note key={note.id} note={note} />
