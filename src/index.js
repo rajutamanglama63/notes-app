31eae06c620bce8ae067672ccaee143c11ca2f92
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-07-19 14:53:18 +0545

    useEffect hook used

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import axios from "axios";
+// import axios from "axios";
 import App from "./App";
 
 // const notes = [
@@ -24,17 +24,36 @@ import App from "./App";
 //   },
 // ];
 
-axios
-  .get("http://localhost:5000/notes")
-  .then((response) => {
-    console.dir(response);
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(
-      <React.StrictMode>
-        <App notes={response.data} />
-      </React.StrictMode>
-    );
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+// const res = axios.get("http://localhost:5000/notes")
+// console.log(res)  ------> this gives the promise object
+
+// res.then((result) => {
+//   console.log(result.data)
+// }) -------------->  this will be execute only after web api completed successfully
+
+// res.catch((err) => {
+// console.log(err.message)
+// }) ---------> if there is issue or bug in our url or i mean there is invalid url it help to handle that error tactfully
+
+// this code below runs perfectly when we send props to App component
+// axios
+//   .get("http://localhost:5000/notes")
+//   .then((response) => {
+//     console.dir(response);
+//     const root = ReactDOM.createRoot(document.getElementById("root"));
+//     root.render(
+//       <React.StrictMode>
+//         <App notes={response.data} />
+//       </React.StrictMode>
+//     );
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
